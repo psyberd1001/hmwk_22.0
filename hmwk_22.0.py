d768835762c3d45cb017f37f27a468c562c43bce
psyberd1001 <108468415+psyberd1001@users.noreply.github.com>
2024-11-02 15:22:17 +0300

    new!

--- hmwk_22.0.py
+++ hmwk_22.0.py
@@ -5,20 +5,18 @@ def personal_sum(*numbers):
         for j in range(len(i)):
             try:
                 result = result + i[j]
-            except TypeError:
+            except TypeError as exc:
                 print(f'{i[j]} - ошибка, {incorrect_data + 1} - количество ошибок')
-                incorrect_data += 1
-                continue
+            incorrect_data += 1
+            continue
+
     return tuple((result, incorrect_data))
-
-
-
 
 
 def calculate_average(*numbers):
     results_sum = 0
-    results = personal_sum(*numbers)
     try:
+        results = personal_sum(*numbers)
         for line in numbers:
             for i in range(len(line)):
                 if type(line[i]) == int or type(line[i]) == float:
@@ -32,7 +30,7 @@ def calculate_average(*numbers):
     return results_sum
 
 
-
 print(f'Результат 1: {calculate_average("1, 2, 3")}')
 print(f'Результат 2: {calculate_average(["Строка", 1, 3, "Ещё Строка"])}')
+print(f'Результат 3: {calculate_average(567)}')
 print(f'Результат 4: {calculate_average([42, 15, 36, 13])}')
